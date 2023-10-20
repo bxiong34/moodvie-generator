c4619d220eb5bed828ce663d2dfe6a8188bd598b
Za Matheson <zamatheson@Zas-MacBook-Air.lan>
2023-10-19 20:24:08 -0500

    watchlist

---
+++ assets/js/watchlist.js
@@ -0,0 +1,69 @@
+function addMovieToWatchlist(movie) {
+    const container = document.querySelector(".watchlist-container");
+    const movieItem = document.createElement("div");
+    movieItem.className = "movie-item";
+    
+    
+    const titleElement = document.createElement("li");
+    titleElement.textContent = movie.title + " " + movie.rating
+    movieItem.appendChild(titleElement);
+    
+    
+    container.appendChild(movieItem);
+    }
+    
+    
+    document.getElementById("addMovie").addEventListener("click", function() {
+    console.log("button clicked");
+    const movieName = document.getElementById("movieInput").value;
+    const rating = document.querySelectorAll(".starRating").length;
+    
+    
+    if (movieName) {
+    var movies = JSON.parse(localStorage.getItem("movieList")) || [];
+    
+    
+    const movie = {
+    title: movieName,
+    rating: rating
+    };
+    
+    
+    movies.push(movie);
+    localStorage.setItem("movieList", JSON.stringify(movies));
+    document.getElementById("movieInput").value = "";
+    
+    
+    alert("Moodvie has been added to your watchlist!");
+    
+    
+    addMovieToWatchlist(movie);
+    } else {
+    alert("Enter a movie title");
+    }
+    });
+    
+    
+    window.addEventListener("load", function() {
+    var movies = JSON.parse(localStorage.getItem("movieList")) || [];
+    
+    
+    for (const movie of movies) {
+    addMovieToWatchlist(movie);
+    }
+    });
+    
+    
+    var stars = document.querySelectorAll(".starrating a");
+    
+    
+    stars.forEach((item, index1) => {
+    item.addEventListener("click", () => {
+    stars.forEach((star, index2) => {
+    index1 >= index2 ? star.classList.add("active");
+    })
+    })
+    })
+    
+    
+    
