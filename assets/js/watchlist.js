37a6d5d03109ef1630fd7f66be28bd119c41b498
Za Matheson <zamatheson@Zas-MacBook-Air.lan>
2023-10-24 16:58:18 -0500

    added comments to the watchlist code files

--- assets/js/watchlist.js
+++ assets/js/watchlist.js
@@ -1,3 +1,4 @@
+// this allows the movie and star rating that the user entered to appear in the watchlist
 function addMovieToWatchlist(movie) {
     const container = document.querySelector(".watchlist-container");
     const movieItem = document.createElement("div");
@@ -10,6 +11,7 @@ function addMovieToWatchlist(movie) {
     container.appendChild(movieItem);
 }
 
+// this saves the movies for the watchlist into local storage
 document.getElementById("addMovie").addEventListener("click", function() {
     const movieName = document.getElementById("movieInput").value;
     const rating = document.querySelectorAll(".star-rating .active").length;
@@ -27,6 +29,7 @@ document.getElementById("addMovie").addEventListener("click", function() {
         localStorage.setItem("movieList", JSON.stringify(movies));
         document.getElementById("movieInput").value = "";
 
+// this is the modal
         addMovieToWatchlist(movie);
         modal.style.display = "block";
     } else {
@@ -44,7 +47,7 @@ document.getElementById("addMovie").addEventListener("click", function() {
     }, 1000);
 });
 
-
+// this controls the stars color change when the mouse scrolls over, as well as documenting which stars have been chosen by the user
 const stars = document.querySelectorAll(".star-rating i");
 
 stars.forEach((star, index) => {
@@ -67,6 +70,7 @@ stars.forEach((star, index) => {
     });
 });
 
+// this makes the stars change color when the mouse scrolls away from them
 document.querySelector(".star-rating").addEventListener("mouseleave", () => {
     const rating = document.querySelectorAll(".star-rating .active").length;
     if (rating === 0) {
@@ -74,6 +78,7 @@ document.querySelector(".star-rating").addEventListener("mouseleave", () => {
     }
 });
 
+// this saves the movies for the watchlist into local storage
 window.addEventListener("load", function() {
     var movies = JSON.parse(localStorage.getItem("movieList")) || [];
 
